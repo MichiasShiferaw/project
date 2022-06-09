1cf19890f3b8cca1dcbab5233e00208299ed047e
Michias Shiferaw <mshif073@uottawa.ca>
2022-06-09 14:20:56 -0400

    Icons Modified

--- src/Services/ServiceCard.js
+++ src/Services/ServiceCard.js
@@ -9,9 +9,16 @@ function ServiceCard () {
                 <div className="card bg-secondary text-light">
                     <div className="card-body text-center">
                         <div className="h1 mb-3">
-                            <i className="bi bi-laptop"></i>
+<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="76.52pt" height="35pt" viewBox="0 0 739.000000 338.000000" preserveAspectRatio="xMidYMid meet">
+
+<g transform="translate(0.000000,338.000000) scale(0.100000,-0.100000)" fill="#FFFFFF" stroke="none">
+<path d="M1860 3013 c-74 -38 -100 -124 -60 -202 31 -62 63 -71 244 -71 l156 0 0 -235 0 -235 -461 0 c-322 0 -473 -4 -500 -12 -51 -15 -114 -69 -142 -122 l-22 -41 0 -865 0 -865 22 -41 c12 -22 38 -55 58 -72 l36 -32 1738 0 1738 0 33 23 c26 17 167 243 630 1016 l597 994 98 49 c53 27 102 47 109 45 6 -3 128 -105 271 -227 147 -125 282 -232 310 -246 117 -56 258 -4 319 118 46 92 32 161 -50 239 -81 76 -644 550 -695 585 -57 38 -117 49 -181 32 -26 -7 -509 -242 -1073 -522 -895 -445 -1027 -508 -1038 -494 -6 8 -64 85 -128 169 -118 156 -171 213 -231 246 -30 17 -77 18 -593 23 l-560 5 0 230 0 230 165 5 c110 3 173 9 188 18 37 21 62 72 62 125 0 57 -19 97 -57 123 -26 18 -55 19 -488 22 -423 2 -463 1 -495 -15z m3278 -1175 c-8 -13 -167 -277 -353 -588 -186 -311 -345 -573 -353 -582 -14 -17 -96 -18 -1454 -18 l-1438 0 0 580 0 580 945 0 945 0 108 -137 c311 -395 290 -376 412 -370 l75 3 555 277 c305 152 558 277 563 277 5 0 3 -10 -5 -22z"/>
+<path d="M1065 2909 c-32 -18 -765 -751 -781 -781 -33 -63 -6 -149 58 -188 40 -24 105 -26 138 -4 41 27 760 747 776 776 29 56 13 127 -40 177 -30 28 -117 39 -151 20z"/>
+<path d="M6771 1502 c-84 -145 -217 -417 -240 -489 -29 -91 -23 -145 22 -205 88 -116 245 -155 401 -102 93 32 176 135 176 220 0 38 -31 127 -86 243 -58 123 -215 411 -224 411 -3 0 -25 -35 -49 -78z"/>
+</g>
+</svg>
                         </div>
-                        <h3 className="card-title mb-3">Service 1</h3>
+                        <h3 className="card-title mb-3">Oil Change</h3>
                         <p className="card-text">
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                             Iure, quas quidem possimus dolorum esse eligendi?
@@ -24,9 +31,9 @@ function ServiceCard () {
                 <div className="card bg-dark text-light">
                     <div className="card-body text-center">
                         <div className="h1 mb-3">
-                            <i className="bi bi-laptop"></i>
+                            <i class="bi bi-fan"></i>
                         </div>
-                        <h3 className="card-title mb-3">Service 2</h3>
+                        <h3 className="card-title mb-3">Air Filters</h3>
                         <p className="card-text">
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                             Iure, quas quidem possimus dolorum esse eligendi?
@@ -41,7 +48,7 @@ function ServiceCard () {
                         <div className="h1 mb-3">
                             <i className="bi bi-person-square"></i>
                         </div>
-                        <h3 className="card-title mb-3">Service 3</h3>
+                        <h3 className="card-title mb-3">Engine Tune-Up</h3>
                         <p className="card-text">
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                             Iure, quas quidem possimus dolorum esse eligendi?
@@ -54,9 +61,9 @@ function ServiceCard () {
                 <div className="card bg-dark text-light">
                     <div className="card-body text-center">
                         <div className="h1 mb-3">
-                            <i className="bi bi-people"></i>
+                            <i class="bi bi-currency-dollar"></i>
                         </div>
-                        <h3 className="card-title mb-3">Service 4</h3>
+                        <h3 className="card-title mb-3">Tire Purchase</h3>
                         <p className="card-text">
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                             Iure, quas quidem possimus dolorum esse eligendi?
@@ -75,9 +82,9 @@ function ServiceCard () {
                         <div className="card bg-dark text-light">
                             <div className="card-body text-center">
                                 <div className="h1 mb-3">
-                                    <i className="bi bi-laptop"></i>
+                                    <i class="bi bi-currency-dollar"></i>
                                 </div>
-                                <h3 className="card-title mb-3">Service 5</h3>
+                                <h3 className="card-title mb-3">Tire Installation</h3>
                                 <p className="card-text">
                                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                                     Iure, quas quidem possimus dolorum esse eligendi?
@@ -92,7 +99,7 @@ function ServiceCard () {
                                 <div className="h1 mb-3">
                                     <i className="bi bi-person-square"></i>
                                 </div>
-                                <h3 className="card-title mb-3">Service 6</h3>
+                                <h3 className="card-title mb-3">Brakes</h3>
                                 <p className="card-text">
                                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                                     Iure, quas quidem possimus dolorum esse eligendi?
@@ -105,9 +112,18 @@ function ServiceCard () {
                         <div className="card bg-dark text-light">
                             <div className="card-body text-center">
                                 <div className="h1 mb-3">
-                                    <i className="bi bi-people"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="35.000000pt" height="35.000000pt" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
+
+<g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#FFFFFF" stroke="none">
+<path d="M919 4696 c-88 -31 -139 -79 -178 -166 -18 -40 -21 -66 -21 -237 l0 -193 100 0 100 0 0 164 c0 91 4 176 10 191 19 50 53 55 350 55 297 0 331 -5 350 -55 6 -15 10 -100 10 -191 l0 -164 100 0 100 0 0 193 0 194 -31 61 c-32 65 -66 99 -133 135 -40 21 -52 22 -381 24 -261 2 -348 0 -376 -11z"/>
+<path d="M3479 4696 c-88 -31 -139 -79 -178 -166 -18 -40 -21 -66 -21 -237 l0 -193 100 0 100 0 0 164 c0 91 4 176 10 191 19 50 53 55 350 55 297 0 331 -5 350 -55 6 -15 10 -100 10 -191 l0 -164 100 0 100 0 0 193 0 194 -31 61 c-32 65 -66 99 -133 135 -40 21 -52 22 -381 24 -261 2 -348 0 -376 -11z"/>
+<path d="M366 3984 c-82 -20 -166 -67 -227 -129 -65 -66 -94 -112 -119 -194 -20 -62 -20 -95 -20 -1461 0 -1344 1 -1401 19 -1459 55 -178 216 -308 411 -331 56 -7 800 -10 2190 -8 l2105 3 63 23 c153 55 257 157 308 301 l24 66 0 1401 c0 1365 -1 1404 -20 1465 -49 160 -184 284 -351 324 -94 22 -4291 21 -4383 -1z m4392 -230 c29 -14 68 -44 87 -66 70 -82 66 15 63 -1504 l-3 -1369 -31 -55 c-33 -60 -99 -112 -162 -130 -51 -14 -4253 -14 -4304 0 -63 18 -129 70 -162 130 l-31 55 -3 1369 c-3 1519 -7 1422 63 1504 19 22 58 52 87 66 l52 26 2146 0 2146 0 52 -26z"/>
+<path d="M3690 2560 l0 -200 -205 0 -205 0 0 -155 0 -155 205 0 205 0 0 -205 0 -205 150 0 150 0 0 205 0 205 205 0 205 0 0 155 0 155 -205 0 -205 0 0 200 0 200 -150 0 -150 0 0 -200z"/>
+<path d="M720 2205 l0 -155 560 0 560 0 0 155 0 155 -560 0 -560 0 0 -155z"/>
+</g>
+</svg>
                                 </div>
-                                <h3 className="card-title mb-3">Service 7</h3>
+                                <h3 className="card-title mb-3">Battery Installation</h3>
                                 <p className="card-text">
                                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                                     Iure, quas quidem possimus dolorum esse eligendi?
@@ -121,9 +137,9 @@ function ServiceCard () {
                         <div className="card bg-secondary text-light">
                             <div className="card-body text-center">
                                 <div className="h1 mb-3">
-                                    <i className="bi bi-people"></i>
+                                    <i class="bi bi-plus-lg"></i>
                                 </div>
-                                <h3 className="card-title mb-3">Service 8</h3>
+                                <h3 className="card-title mb-3">Other</h3>
                                 <p className="card-text">
                                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                                     Iure, quas quidem possimus dolorum esse eligendi?
